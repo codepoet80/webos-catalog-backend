d9dc6d052a50dbd727fcf4ab69dd76d88777c744
codepoet80 <curator@webosarchive.com>
2020-12-04 14:08:51 -0500

    try new way to do latest

---
+++ latest.php
@@ -0,0 +1,15 @@
+$latest = "com.palm.app-museum2_2.7.2_all.ipk";
+$attachment_location = $_SERVER["DOCUMENT_ROOT"] . "/" . $latest;
+if (file_exists($attachment_location)) {
+
+    header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
+    header("Cache-Control: public"); // needed for internet explorer
+    header("Content-Type: application/octet-stream");
+    header("Content-Transfer-Encoding: Binary");
+    header("Content-Length:".filesize($attachment_location));
+    header("Content-Disposition: attachment; filename=". $latest);
+    readfile($attachment_location);
+    die();        
+} else {
+    die("Error: File not found.");
+} 

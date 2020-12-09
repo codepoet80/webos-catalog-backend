96a939bc6d1479abc81557d20fd0e3d204ffd0b2
codepoet80 <jonathan.m.wise@gmail.com>
2020-12-08 22:56:03 -0500

    web-based detail page improvements

--- showMuseumDetails.php
+++ showMuseumDetails.php
@@ -1,4 +1,5 @@
-<?PHP
+<html>
+<?php
 $config = include('WebService/config.php');
 
 $string = file_get_contents("extantAppData.json");
@@ -20,9 +21,7 @@ $found_app;
 foreach ($json_a as $this_app => $app_a) {
 	if (strtolower($app_a["title"]) == $search_str || $app_a["id"] == $search_str) {
 		$found_app = $app_a;
-		$app_title = $app_a["title"];
-		$icon_path = "http://" . $config["package_host"] . "/AppImages/" . $app_a["appIconBig"];
-		$found_id = $app_a["id"];
+		$found_id = $found_app["id"];
 	}
 }
 
@@ -37,50 +36,50 @@ $content = stream_get_contents($meta_file);
 fclose($meta_file);
 
 $app_detail = json_decode($content, true);
-$lastVersionNote = $json_m["versionNote"];
-$lastVersionNote = explode("\r\n", $lastVersionNote);
-$lastVersionNote =  $lastVersionNote[count($lastVersionNote)-1];
 $downloadURI = "http://" . $config["package_host"] . "/AppPackages/" . $app_detail["filename"];
+$imgPath = "http://" . $config["package_host"] . "/AppImages/";
 
-$outputObj = array (
-	"version" => $app_detail["version"],
-	"versionNote" => $lastVersionNote,
-	"lastModifiedTime" => $app_detail["lastModifiedTime"],
-	"downloadURI" => "http://" . $config["package_host"] . "/AppPackages/" . $app_detail["filename"],
-);
-
-echo ("<img src='" . $icon_path . "' style='width:128px;height:128px;float:right;'>");
-echo ("<h1>" . $app_title . "</h1>");
 ?>
-<table>
-<tr><td>Museum ID</td><td><?php echo $found_app["id"] ?></td></tr>
-<tr><td>Application ID</td><td><?php echo $app_detail["publicApplicationId"] ?></td></tr>
-<tr><td>Author</td><td><?php echo "<a href='" . $app_detail["homeURL"] . "'>" . $found_app["author"] . "</a>"?></td></tr>
-<tr><td>Version</td><td><?php echo $app_detail["version"] ?></td></tr>
-<tr><td>Description</td><td><?php echo $app_detail["description"] ?></td></tr>
-<tr><td>Version Note</td><td><?php echo $app_detail["versionNote"] ?></td></tr>
-<tr><td>Download</td><td><?php echo $downloadURI ?></td></tr>
-<tr><td>Device Support</td>
+<head>
+<title><?php echo $found_app["title"] ?> - webOS App Museum II</title>
+<link rel="stylesheet" href="webmuseum.css">
+</head>
+<body>
+<table border="0" >
+<tr><td colspan="2"><h1><?php echo $found_app["title"] ?></h1></td>
+	<td rowspan="2">
+	<img src="<?php echo $imgPath. $found_app["appIconBig"]?>" class="appIcon" >
+</td></tr>
+<tr><td class="rowTitle">Museum ID</td><td><?php echo $found_app["id"] ?></td></tr>
+<tr><td class="rowTitle">Application ID</td><td colspan="2"><?php echo $app_detail["publicApplicationId"] ?></td></tr>
+<tr><td class="rowTitle">Author</td><td colspan="2"><?php echo "<a href='" . $app_detail["homeURL"] . "'>" . $found_app["author"] . "</a>"?></td></tr>
+<tr><td class="rowTitle">Version</td><td><?php echo $app_detail["version"] ?></td><td></td></tr>
+<tr><td class="rowTitle">Description</td><td colspan="2"><?php echo str_replace("\r\n", "<br>", $app_detail["description"]) ?></td></tr>
+<tr><td class="rowTitle">Version Note</td><td colspan="2"><?php echo str_replace("\r\n", "<br>", $app_detail["versionNote"]) ?></td></tr>
+<tr><td class="rowTitle">Download</td><td colspan="2"><a href="<?php echo $downloadURI ?>" target="_blank">Direct Link</a></td></tr>
+<tr><td class="rowTitle">Device Support</td>
 <td>
 	<ul>
-	<li>Pre: <?php echo $found_app["Pre"] ?>
-	<li>Pixi: <?php echo $found_app["Pixi"] ?>
-	<li>Pre2: <?php echo $found_app["Pre2"] ?>
-	<li>Veer: <?php echo $found_app["Veer"] ?>
-	<li>Pre3: <?php echo $found_app["Pre3"] ?>
-	<li>TouchPad: <?php echo $found_app["TouchPad"] ?>
+	<li class="deviceSupport<?php echo $found_app["Pre"] ?>">Pre: 
+	<li class="deviceSupport<?php echo $found_app["Pixi"] ?>">Pixi: 
+	<li class="deviceSupport<?php echo $found_app["Pre2"] ?>">Pre2: 
+	<li class="deviceSupport<?php echo $found_app["Veer"] ?>">Veer:
+	<li class="deviceSupport<?php echo $found_app["Pre3"] ?>">Pre3:
+	<li class="deviceSupport<?php echo $found_app["TouchPad"] ?>">TouchPad:
 	</ul>
 </td>
+<td></td>
 </tr>
-<tr><td>Screenshots</td><td>
+<tr><td class="rowTitle">Screenshots</td>
+<td colspan="2">
 <?php
 foreach ($app_detail["images"] as $value) {
-	$imgPath = "http://" . $config["package_host"] . "/AppImages/";
-    echo("<a href='" . $imgPath . $value["screenshot"] . "'><img src='" . $imgPath. $value["thumbnail"] . "' style='width:64px'></a>");
+    echo("<a href='" . $imgPath . $value["screenshot"] . "' target='_blank'><img class='screenshot' src='" . $imgPath. $value["thumbnail"] . "' style='width:64px'></a>");
 }
 ?>
 </td></tr>
-<tr><td>Copyright</td><td><?php echo $app_detail["copyright"] ?></td></tr>
+<tr><td class="rowTitle">License</td><td colspan="2"><?php echo $app_detail["licenseURL"] ?></td></tr>
+<tr><td class="rowTitle">Copyright</td><td colspan="2"><?php echo $app_detail["copyright"] ?></td></tr>
 </table>
 
 <div style="display:none;margin-top:18px">
@@ -88,3 +87,5 @@ foreach ($app_detail["images"] as $value) {
 echo (json_encode($app_a) . "<br><br>");
 echo $content;
 ?>
+</body>
+</html>

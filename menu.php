d69fa64962804cf338f40b137ff0eaccb3e80677
CodePoet80 <jonathan.m.wise@gmail.com>
2021-11-26 11:01:27 -0500

    adhere to menu protocol

--- menu.php
+++ menu.php
@@ -1,3 +1,9 @@
 <?php
-echo file_get_contents("http://www.webosarchive.com/menu.php?content=appcatalog");
+//Figure out what protocol the client wanted
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $protocol = "https://";
+else
+    $protocol = "http://";
+
+echo file_get_contents($protocol."www.webosarchive.com/menu.php?content=appcatalog");
 ?>

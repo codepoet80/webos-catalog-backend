9bb0d5d4ac38d9f8da919fa6bf84baa47cd5c160
codepoet80 <jonathan.m.wise@gmail.com>
2022-01-18 17:31:17 -0500

    better latest

--- latest.php
+++ latest.php
@@ -3,6 +3,7 @@ $config = include('WebService/config.php');
 
 //Get the app info
 $download_path = "http://" . $config["package_host"] . "/AppPackages/";
+
 $meta_path = "http://" . $config["metadata_host"] . "/0.json";
 $meta_file = fopen($meta_path, "rb");
 $content = stream_get_contents($meta_file);
@@ -10,18 +11,6 @@ fclose($meta_file);
 $outputObj = json_decode($content, true);
 
 $attachment_location = $download_path . $outputObj["filename"];
-//echo $attachment_location;
-//if (file_exists($attachment_location)) {
+header("Location: $attachment_location");
 
-    header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
-    header("Cache-Control: public"); // needed for internet explorer
-    header("Content-Type: application/octet-stream");
-    header("Content-Transfer-Encoding: Binary");
-    header("Content-Length:".filesize($attachment_location));
-    header("Content-Disposition: attachment; filename=". $outputObj["filename"]);
-    readfile($attachment_location);
-    die();        
-//} else {
-    //die("Error: File not found.");
-//} 
 ?>

6a58af5652e8986f0c7e353a659f5f41a661707e
codepoet80 <jon.wise@jonandnic.com>
2022-01-18 16:54:06 -0500

    debug

--- latest.php
+++ latest.php
@@ -10,6 +10,7 @@ fclose($meta_file);
 $outputObj = json_decode($content, true);
 
 $attachment_location = $download_path . $outputObj["filename"];
+echo $attachment_location;
 if (file_exists($attachment_location)) {
 
     header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");

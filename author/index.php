1f12386b0db2619120ab789e88a65f8700e1a435
codepoet80 <jonathan.m.wise@gmail.com>
2022-01-19 22:30:16 -0500

    try new link

--- author/index.php
+++ author/index.php
@@ -136,7 +136,7 @@ $homePath = "showMuseum.php?" . http_build_query($query);
 	<?php
 		echo("<table cellpadding='5'>");
 		foreach($app_response["data"] as $app) {
-			echo("<tr><td align='center' valign='top'><a href='showMuseumDetails.php?{$_SERVER["QUERY_STRING"]}&app={$app["id"]}'><img style='width:64px; height:64px' src='{$img_path}{$app["appIcon"]}' border='0'></a>");
+			echo("<tr><td align='center' valign='top'><a href='{$protocol}{$config["service_host"]}showMuseumDetails.php?{$_SERVER["QUERY_STRING"]}&app={$app["id"]}'><img style='width:64px; height:64px' src='{$img_path}{$app["appIcon"]}' border='0'></a>");
 			echo("<td width='100%' style='padding-left: 14px'><b><a href='../showMuseumDetails.php?{$_SERVER["QUERY_STRING"]}&app={$app["id"]}'>{$app["title"]}</a></b><br/>");
 			echo("<small>" . substr($app["summary"],0, 180) . "...</small><br/>&nbsp;");
 			echo("</td></tr>");

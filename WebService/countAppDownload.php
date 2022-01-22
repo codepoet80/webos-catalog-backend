73c1015e035efef488686b754f6d39b55fb455f3
codepoet80 <jonathan.m.wise@gmail.com>
2022-01-22 12:23:39 -0500

    add logging

--- WebService/countAppDownload.php
+++ WebService/countAppDownload.php
@@ -1,3 +1,29 @@
 <?php
-error_log("Don't know how to record download of appid: " . $_GET["appid"]);
+if (isset($_GET["appid"]) && $_GET["appid"] != "") {
+    //Try to prepare the logs
+    $logpath = null;
+    try {
+        clearstatcache();
+        $logpath = "logs";
+            if (!file_exists($logpath)) {
+                    mkdir($logpath, 0774, true);
+            }
+            $logpath = getcwd() . "/" . $logpath . "/downloadcount.log";
+            if (!file_exists($logpath)) {
+                    $logfile=fopen($logpath, "x");
+                    fwrite($logfile, "TimeStamp,AppId".PHP_EOL);
+                    fclose($logfile);
+            }
+    } catch (exception $e) {
+        //Fail with web server log and move on
+        unset($logpath);
+        error_log("Non-fatal error: " . $_SERVER ['SCRIPT_NAME'] . " was unable to create a log file. Check directory permissions for web server user.", 0);
+    }
+
+    if (file_exists($logpath)) {
+        $timestamp = date('Y/m/d H:i:s');
+        $logdata = $timestamp . "," . $_GET["appid"]. PHP_EOL;
+        file_put_contents($logpath, $logdata, FILE_APPEND);
+    }
+}
 ?> 

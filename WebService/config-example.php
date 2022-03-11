010452ca0a85351759f50c139259789a707446fe
codepoet80 <jon.wise@jonandnic.com>
2022-03-11 13:57:06 -0500

    update config example

--- WebService/config-example.php
+++ WebService/config-example.php
@@ -1,10 +1,11 @@
 <?php
 return array(
 
-//put global config here (no secrets)
-'package_host' => 'packages.webosarchive.com',
-'service_host' => 'appcatalog.webosarchive.com',
-'metadata_host' => 'appmetadata.webosarchive.com',
-'image_host' => 'packages.webosarchive.com/AppImages'
+//put global config here, no trailing slahes
+// (and no secrets, the contents of this file are available via an API call to any client)
+'package_host' => 'packages.myhost.com',
+'service_host' => 'appcatalog.myhost.com',
+'metadata_host' => 'appmetadata.myhost.com',
+'image_host' => 'appcatalog.myhost.com/AppImages'
 );
 ?>

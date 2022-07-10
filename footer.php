cc1fb89f0d9f59192462b23cda80f5961442c310
codepoet80 <jonathan.m.wise@gmail.com>
2022-07-10 15:46:46 -0400

    update footer

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <p style="clear:both;margin-top:60px;" class="legal">
-<a href="http://webosarchive.com">webOS Archive</a> provides this listing without profit and under Fair Use provisions as a digital archive for historical purposes. If you have questions or comments, or to submit a take down request for IP you own the rights for, please <a href="javascript:document.location=atob('bWFpbHRvOmN1cmF0b3JAd2Vib3NhcmNoaXZlLmNvbQ==')">email the curator</a>.
+<a href="http://webosarchive.com">webOS Archive</a> provides this listing without profit and under Fair Use provisions as a digital archive for historical purposes. If you have questions or comments, or to submit a take down request for IP you own the rights to, please <a href="javascript:document.location=atob('bWFpbHRvOmN1cmF0b3JAd2Vib3NhcmNoaXZlLmNvbQ==')">email the curator</a>.
 </p>

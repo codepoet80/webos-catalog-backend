6d399e2beabe919cf3b8f4224822d6468f95c34e
codepoet80 <curator@webosarchive.com>
2022-07-18 09:37:58 -0400

    update example config

--- WebService/config-example.php
+++ WebService/config-example.php
@@ -1,14 +1,23 @@
 <?php
-return array(
 //copy this file to config.php
+//  put global config here, subdirectories are supported, but no trailing slahes
+//  you must host these repositories over HTTPS AND HTTP without redirecting to HTTPS
+//  (you can use the Upgrade-Insecure-Requests header in your server config to serve HTTPS to modern web clients)
+//  Note: the contents of this file are available via an API call to any client, do not embed any secrets
 
-//put global config here, subdirectories are supported, but no trailing slahes
-//you must host these repositories over HTTP without redirecting to HTTPS -- webOS has poor support for HTTPS
-//you can use the Upgrade-Insecure-Requests header in your server config to serve HTTPS to modern web clients
-//(Note: the contents of this file are available via an API call to any client, do not embed any secrets)
-'package_host' => 'packages.myhost.com',
-'service_host' => 'appcatalog.myhost.com',
-'metadata_host' => 'appmetadata.myhost.com',
-'image_host' => 'appcatalog.myhost.com/AppImages'
+$image_mirrors = array('imagemirror1.com/AppImages', 'imagemirror2.com/AppImages');
+$package_mirrors = array('packagemirror1.com/AppPackages', 'packagemirror2.com/AppPackages');
+
+return array(
+    'service_host' => 'appcatalog.myhost.com',
+    'metadata_host' => 'appmetadata.myhost.com',
+    'image_host' => select_lb_resource($image_mirrors),
+    'package_host' => select_lb_resource($package_mirrors)
 );
+
+//TODO: If we had a state machine, we could be more sophisticated.
+//  For now, just pick a random resource each time
+function select_lb_resource($resource_array) {
+    return($resource_array[array_rand($resource_array)]);
+}
 ?>
